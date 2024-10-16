df2b33ae5a2aba87d1536b7b2be70798a91d768e
D.Danailov <ddanailov.dev@gmail.com>
2024-10-16 20:09:23 +0300

    colors are changed

--- bg_change/src/App.jsx
+++ bg_change/src/App.jsx
@@ -1,16 +1,55 @@
+import { useState } from "react";
 import "./App.css";
 
+const colors = [
+  "#ed8484",
+  "#70c2f4",
+  "#acc999",
+  "#e2c24a",
+  "#db88e8",
+  "#bdc9c9",
+];
+
 function App() {
+  const [color, setColor] = useState(colors[2]);
+
   return (
-    <div className="w-full h-screen duration-200">
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
       <div className="fixed flex flex-wrap justify-center bottom-14 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg">
-            test
-          </button>
-          <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg">
-            test 2
-          </button>
+          <button
+            onClick={() => setColor(colors[0])}
+            className="outline-none px-5 py-2 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: colors[0] }}
+          ></button>
+          <button
+            onClick={() => setColor(colors[1])}
+            className="outline-none px-5 py-2 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: colors[1] }}
+          ></button>
+          <button
+            onClick={() => setColor(colors[2])}
+            className="outline-none px-5 py-2 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: colors[2] }}
+          ></button>
+          <button
+            onClick={() => setColor(colors[3])}
+            className="outline-none px-5 py-2 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: colors[3] }}
+          ></button>
+          <button
+            onClick={() => setColor(colors[4])}
+            className="outline-none px-5 py-2 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: colors[4] }}
+          ></button>
+          <button
+            onClick={() => setColor(colors[5])}
+            className="outline-none px-5 py-2 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: colors[5] }}
+          ></button>
         </div>
       </div>
     </div>
@@ -18,3 +57,5 @@ function App() {
 }
 
 export default App;
+
+// https://www.youtube.com/watch?v=Tu9_vmzEK9w&list=PLRAV69dS1uWQos1M1xP6LWN6C-lZvpkmq&index=9
